01e008691c7a1fb706a6bed14b2b1a8fb63ebe2c
jeffkim <jeffkim207@gmail.com>
2019-12-17 12:35:38 +0300

    Exporting project arms from REDCap API using json and csv formats.

---
+++ export_arms.py
@@ -0,0 +1,44 @@
+#####################################################################
+##                                                                 ##
+##       EXPORT WITH JSON FORMAT AND WRITE TO FILE                  ##
+##                                                                 ##
+#####################################################################
+
+import requests
+
+data = {
+    'token': ['api_token_here'],
+    'content': 'arm',
+    'format': 'json',
+    'returnFormat': 'json'
+}
+
+r = requests.post(['api_url_here'], data)
+print(r.text)
+
+with open('arms.json', 'w') as file:
+    file.write(r.text)
+
+
+#####################################################################
+##                                                                 ##
+##       EXPORT WITH CSV FORMAT AND WRITE TO FILE                  ##
+##                                                                 ##
+#####################################################################
+import requests
+import pandas as pd
+from io import StringIO
+
+data = {
+    'token': ['api_token_here'],
+    'content': 'arm',
+    'format': 'csv',
+    'returnFormat': 'csv'
+}
+
+r = requests.post(['api_url_here'], data)
+df = pd.read_csv(StringIO(r.text))
+print(df)
+
+with open('arms.csv', 'w') as file:
+    file.write(r.text)
