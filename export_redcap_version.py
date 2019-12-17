b18b57baad68d284ce76fe64acbd055f3d211d8c
jeffkim <jeffkim207@gmail.com>
2019-12-17 14:52:58 +0300

    Exporting REDCap API version.

---
+++ export_redcap_version.py
@@ -0,0 +1,11 @@
+import requests
+
+data = {
+    'token': ['api_token_here'],
+    'content': 'version',
+    'format': 'json',
+    'returnFormat': 'json'
+}
+
+r = requests.post(['api_url_here'], data)
+print(r.text)
