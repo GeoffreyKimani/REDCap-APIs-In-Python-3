7f3a37ba952a7c82c1ae1dfb960c1793cb697063
jeffkim <jeffkim207@gmail.com>
2019-12-18 10:44:32 +0300

    Exporting survey return code from REDCap API using json and csv formats.

---
+++ export_survey_return_code.py
@@ -0,0 +1,54 @@
+"""
+#####################################################################
+##                                                                 ##
+##       EXPORT WITH JSON FORMAT AND WRITE TO FILE                 ##
+##                                                                 ##
+#####################################################################
+"""
+
+import requests
+
+data = {
+    'token': ['api_token_here'],
+    'content': 'surveyReturnCode',
+    'record': ['record_id'],
+    'instrument': ['instrument_name'],
+    'event': ['event_arm'],
+    'format': 'json',
+    'returnFormat': 'json'
+}
+
+r = requests.post(['api_url_here'], data)
+print(r.text)
+
+with open('survey_return_code.json', 'w') as file:
+    file.write(r.text)
+
+"""
+#####################################################################
+##                                                                 ##
+##       EXPORT WITH CSV FORMAT AND WRITE TO FILE                  ##
+##                                                                 ##
+#####################################################################
+"""
+
+import requests
+import pandas as pd
+from io import StringIO
+
+data = {
+    'token': ['api_token_here'],
+    'content': 'surveyReturnCode',
+    'record': ['record_id'],
+    'instrument': ['instrument_name'],
+    'event': ['event_arm'],
+    'format': 'json',
+    'returnFormat': 'json'
+}
+
+r = requests.post(['api_url_here'], data)
+df = pd.read_csv(StringIO(r.text))
+print(df)
+
+with open('survey_return_code.csv', 'w') as file:
+    file.write(r.text)
